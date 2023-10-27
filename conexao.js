7da379dec71551153b570d2ea39f540c9e33b9ef
MaxWillian <max-will-94@hotmail.com>
2023-10-27 11:58:41 -0300

    Iniciando projeto e conexões

---
+++ conexao.js
@@ -0,0 +1,12 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : 'postgres',
+    database : 'knex.js'
+  }
+});
+
+module.exports = knex;
+
