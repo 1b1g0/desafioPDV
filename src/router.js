d0c5deb63ef88dd1b2f10933fb206eeea0a54033
Igor Felipe <contas460@gmail.com>
2023-10-28 17:24:42 -0300

    endpoint listCategories adicionado

--- src/router.js
+++ src/router.js
@@ -3,8 +3,10 @@ const router = express();
 
 const { registerUser } = require('./controller/user.js');
 const loginUser = require('./controller/login.js');
+const { listCategories } = require('./controller/product.js');
 
 router.post('/usuario', registerUser)
 router.post('/login', loginUser)
+router.get('/categoria', listCategories)
 
 module.exports = router;
