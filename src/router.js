0b6bbf6d87587e34aa388c9e87cf722e6197dcd9
Lamonier-Barbosa <lamoni_lr@hotmail.com>
2023-10-28 15:36:38 -0300

    Implementação da função de login do usuário.

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express();
 
 const { registerUser } = require('./controller/user.js');
+const loginUser = require('./controller/login.js');
 
 router.post('/usuario', registerUser)
+router.post('/login', loginUser)
 
 module.exports = router;
