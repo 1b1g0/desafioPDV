078137151daf9b576ac3879c88b9482211a96696
David Silva <david_gui29@hotmail.com>
2023-10-29 00:27:07 -0300

    :bug: fix: Corrige o erro em req.usuario

--- src/middleware/login.js
+++ src/middleware/login.js
@@ -1,33 +1,33 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const hashPassword = require("../database/hashPassword");
-const knex = require('../database/connection/connection.js');
+const knex = require("../database/connection/connection.js");
 
 const verifyToken = async (req, res, next) => {
-    const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-    if (!authorization) {
-        return res.status(401).json({ mensagem: 'Não autorizado!' });
+  if (!authorization) {
+    return res.status(401).json({ mensagem: "Não autorizado!" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const tokenUser = jwt.verify(token, hashPassword);
+
+    const usuario = await knex("usuarios").where("id", tokenUser.id).first();
+
+    if (!usuario) {
+      return res.status(401).json({ mensagem: "Não autorizado" });
     }
 
-    const token = authorization.split(' ')[1];
+    const { senha, ...usuarioLogado } = usuario;
 
-    try {
-        const tokenUser = jwt.verify(token, hashPassword);
+    req.usuario = usuarioLogado;
 
-        const usuario = await knex('usuarios').where('id', tokenUser.id).first();
+    next();
+  } catch (error) {
+    return res.status(401).json({ mensagem: "Não autorizado" });
+  }
+};
 
-        if (!usuario) {
-            return res.status(401).json({ mensagem: 'Não autorizado' });
-        }
-
-        const { senha, ...usuarioLogado } = usuario;
-
-        req.usuario = usuarioLogado;
-
-        next()
-    } catch (error) {
-        return res.status(401).json({ mensagem: 'Não autorizado' });
-    }
-}
-
-module.exports = verifyToken;
+module.exports = { verifyToken };
--- src/router.js
+++ src/router.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express();
 
-const { registerUser, detailUser, editUser } = require('./controller/user.js');
-const loginUser = require('./controller/login.js');
-const { listCategories } = require('./controller/product.js');
+const { registerUser, detailUser, editUser } = require("./controller/user.js");
+const loginUser = require("./controller/login.js");
+const { listCategories } = require("./controller/product.js");
+const { verifyToken } = require("./middleware/login.js");
 
 router.post("/usuario", registerUser);
 router.post("/login", loginUser);
-router.put("/usuario", editUser);
+
+router.use(verifyToken);
+git status
 router.get("/categoria", listCategories);
-
-router.use(verifyToken)
-
-router.get('/usuario', detailUser)
+router.put("/usuario", editUser);
+router.get("/usuario", detailUser);
 
 module.exports = router;
