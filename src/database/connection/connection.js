5c75626dd97ed0b9487f888cb75aa0e77d4eebf7
MaxWillian <max-will-94@hotmail.com>
2023-10-30 16:19:50 -0300

    alguma coisa

--- src/database/connection/connection.js
+++ src/database/connection/connection.js
@@ -5,7 +5,7 @@ const knex = require('knex')({
     user : process.env.DB_USER || 'postgres',
     password : process.env.DB_PASSWORD || 'postgres',
     database : process.env.DB_DATABASE || 'pdv',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT 
   }
 });
 
