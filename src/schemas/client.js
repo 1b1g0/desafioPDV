134ec68a4ffdc777cfe7f3ceb4fd7ce3dc76e511
David Silva <david_gui29@hotmail.com>
2023-11-09 18:42:59 -0300

    :bug: fix: Corrige o codigo status erro em validateBody()

--- src/schemas/client.js
+++ src/schemas/client.js
@@ -1,44 +1,47 @@
-const joi = require('joi');
-
-// const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
+const joi = require("joi");
 
 const clientSchema = joi.object({
-    nome: joi.string().required().messages({
-        'string.base': 'O campo nome deve ser string.',
-        'string.empty': 'O campo nome não pode ser uma string vazia.',
-        'any.required': 'O campo nome é obrigatório.'
-    }),
-    email: joi.string().email().required().messages({
-        'string.base': 'O campo email deve ser string.',
-        'string.empty': 'O campo email não pode ser uma string vazia.',
-        'string.email': 'O campo email deve ter um formato válido.',
-        'any.required': 'O campo email é obrigatório.'
-    }),
-    cpf: joi.string().length(11).required().messages({
-        'string.base': 'O campo cpf deve ser string.',
-        'string.empty': 'O campo cpf não pode ser uma string vazia.',
-        'string.length': 'O campo cpf deve ter 11 caracteres',
-        'any.required': 'O campo cpf é obrigatório.'
-    }),
-    cep: joi.string().length(8).messages({
-        'string.empty': 'O campo cep não pode ser uma string vazia.',
-        'string.length': 'O campo cpf deve ter 8 caracteres',
-    }),
-    rua: joi.string().messages({
-        'string.empty': 'O campo rua não pode ser uma string vazia.',
-    }),
-    numero: joi.string().messages({
-        'string.empty': 'O campo numero não pode ser uma string vazia.',
-    }),
-    bairro: joi.string().messages({
-        'string.empty': 'O campo bairro não pode ser uma string vazia.',
-    }),
-    cidade: joi.string().messages({
-        'string.empty': 'O campo cidade não pode ser uma string vazia.',
-    }),
-    estado: joi.string().messages({
-        'string.empty': 'O campo estado não pode ser uma string vazia.',
-    })
+  nome: joi.string().required().messages({
+    "string.base": "O campo nome deve ser string.",
+    "string.empty": "O campo nome não pode ser uma string vazia.",
+    "any.required": "O campo nome é obrigatório.",
+  }),
+  email: joi.string().email().required().messages({
+    "string.base": "O campo email deve ser string.",
+    "string.empty": "O campo email não pode ser uma string vazia.",
+    "string.email": "O campo email deve ter um formato válido.",
+    "any.required": "O campo email é obrigatório.",
+  }),
+  cpf: joi.string().length(11).required().messages({
+    "string.base": "O campo cpf deve ser string.",
+    "string.empty": "O campo cpf não pode ser uma string vazia.",
+    "string.length": "O campo cpf deve ter 11 caracteres",
+    "any.required": "O campo cpf é obrigatório.",
+  }),
+  cep: joi.string().length(8).messages({
+    "string.base": "O campo cep deve ser string.",
+    "string.empty": "O campo cep não pode ser uma string vazia.",
+    "string.length": "O campo cep deve ter 8 caracteres",
+  }),
+  rua: joi.string().messages({
+    "string.empty": "O campo rua não pode ser uma string vazia.",
+  }),
+  numero: joi.string().messages({
+    "string.base": "O campo nome deve ser string.",
+    "string.empty": "O campo numero não pode ser uma string vazia.",
+  }),
+  bairro: joi.string().messages({
+    "string.base": "O campo nome deve ser string.",
+    "string.empty": "O campo bairro não pode ser uma string vazia.",
+  }),
+  cidade: joi.string().messages({
+    "string.base": "O campo nome deve ser string.",
+    "string.empty": "O campo cidade não pode ser uma string vazia.",
+  }),
+  estado: joi.string().messages({
+    "string.base": "O campo nome deve ser string.",
+    "string.empty": "O campo estado não pode ser uma string vazia.",
+  }),
 });
 
 module.exports = clientSchema;
