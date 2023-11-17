68bfbfcf774ac59b88d3a22fdcac88f7463d8c2e
David Silva <david_gui29@hotmail.com>
2023-11-17 01:55:27 -0300

    :recycle: refactor: Insere o envio de emails na funcionalidade cadastrar pedidos

--- src/controller/order.js
+++ src/controller/order.js
@@ -1,82 +1,85 @@
 const knex = require("../database/connection/connection.js");
-const orderSchema = require('../schemas/order.js')
-
-//const sendEmail = require("../server/")
-
+const transporter = require("../email/transporter.js");
+const orderSchema = require("../schemas/order.js");
 
 //*******************Register Order****************/
 const registerOrder = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
 
   try {
-    await orderSchema.validate(req.body)
-    //const usuarioId = req.usuario.id
-    let erros = []
-    let valorTotal = 0
+    await orderSchema.validate(req.body);
+    let erros = [];
+    let valorTotal = 0;
 
     for (const item of pedido_produtos) {
-      let produtoCorrente = await knex('produtos')
-        .where('id', '=', item.produto_id)
-        .first()
+      let produtoCorrente = await knex("produtos")
+        .where("id", "=", item.produto_id)
+        .first();
 
       if (!produtoCorrente) {
         erros.push({
-          mensagem: `Não existe produto para o produto_id informado: ${item.produto_id}`
-        })
-        continue //verifica o proximo item
+          mensagem: `Não existe produto para o produto_id informado: ${item.produto_id}`,
+        });
+        continue; //verifica o proximo item
       }
 
       if (item.quantidade_produto > produtoCorrente.quantidade_estoque) {
         erros.push({
-          mensagem: `A quantidade solicitada: ${ item.quantidade_produto } para o produto de ID: ${ produtoCorrente.id } é maior que a quantidade em estoque: ${ produtoCorrente.quantidade_estoque }`
-        })
-      continue
+          mensagem: `A quantidade solicitada: ${item.quantidade_produto} para o produto de ID: ${produtoCorrente.id} é maior que a quantidade em estoque: ${produtoCorrente.quantidade_estoque}`,
+        });
+        continue;
+      }
+
+      valorTotal += produtoCorrente.valor * item.quantidade_produto;
+      item.valor_produto = produtoCorrente.valor;
+      item.quantidade_estoque = produtoCorrente.quantidade_estoque;
     }
 
-    valorTotal += produtoCorrente.valor * item.quantidade_produto
-    item.valor_produto = produtoCorrente.valor
-    item.quantidade_estoque = produtoCorrente.quantidade_estoque
-  }
-
     if (erros.length > 0) {
-    return res.status(400).json({ erros })
-  }
+      return res.status(400).json({ erros });
+    }
 
-  //registar pedido ***********
-  const pedido = await knex('pedidos')
-    .insert({
-      
-      observacao,
-      valor_total : valorTotal
-    })
-    .returning('*') //retorna tudo 
-    
-    console.log(pedido[0])
-
-  for (const item of pedido_produtos) {
-    await knex('pedidos_produtos')
+    //registar pedido ***********
+    const pedido = await knex("pedidos")
       .insert({
+        observacao,
+        valor_total: valorTotal,
+      })
+      .returning("*"); //retorna tudo
+
+    console.log(pedido[0]);
+
+    for (const item of pedido_produtos) {
+      await knex("pedidos_produtos").insert({
         pedido_id: pedido[0].id,
         produto_id: item.produto_id,
         quantidade_produto: item.quantidade_produto,
-        valor_produto: item.valor_produto
-      })
+        valor_produto: item.valor_produto,
+      });
 
-    let quantidadeReduzida = item.quantidade_estoque - item.quantidade_produto
+      let quantidadeReduzida =
+        item.quantidade_estoque - item.quantidade_produto;
 
-    await knex('produtos')
-      .where('id', '=', item.produto_id)
-      .update({
-        quantidade_estoque: quantidadeReduzida
-      })
+      await knex("produtos").where("id", "=", item.produto_id).update({
+        quantidade_estoque: quantidadeReduzida,
+      });
+    }
+
+    const client = await knex("clientes").where({ id: cliente_id }).first();
+
+    transporter.sendMail({
+      from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+      to: `${client.nome} <${client.email}>`,
+      subject: "Tentativa de Login",
+      text: `Um pedido foi criado para você, ${client.nome}`,
+    });
+
+    return res.status(201).json({ mensagem: "Pedido gerado com sucesso" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ mensagem: error.message });
   }
-
-  return res.status(201).json({ mensagem: "Pedido gerado com sucesso" })
-} catch (error) {
-  console.log(error.message)
-  return res.status(500).json({ mensagem: error.message })
-}
-}
+};
 
 //*******************List Order****************/
 const listOrder = async (req, res) => {
@@ -85,47 +88,47 @@ const listOrder = async (req, res) => {
     const isNumeric = Number(customerId) == customerId;
     if (!isNumeric) {
       return res.status(400).json({
-        mensagem: `${customerId} não é um id válido.`
-      })
+        mensagem: `${customerId} não é um id válido.`,
+      });
     }
-
   }
   try {
     if (customerId) {
-      const customerCheck = await knex('pedidos')
+      const customerCheck = await knex("pedidos")
         .where("cliente_id", customerId)
         .select("cliente_id")
         .first();
 
       if (!customerCheck) {
         return res.status(400).json({
-          mensagem: `Não há pedidos registrados para o cliente ${customerId}`
-        })
+          mensagem: `Não há pedidos registrados para o cliente ${customerId}`,
+        });
       }
 
       const formatedOrders = [];
 
-      const allOrders = await knex('pedidos').where("cliente_id", customerId);
+      const allOrders = await knex("pedidos").where("cliente_id", customerId);
 
       for (order of allOrders) {
-
-        const orderProducts = await knex('pedidos_produtos')
-          .where('pedido_id', order.id);
+        const orderProducts = await knex("pedidos_produtos").where(
+          "pedido_id",
+          order.id
+        );
 
         const orderOb = { pedido: order, pedido_produtos: orderProducts };
 
         formatedOrders.push(orderOb);
       }
-      return res.status(200).json(formatedOrders)
-    }
-    else {
+      return res.status(200).json(formatedOrders);
+    } else {
       const formatedOrders = [];
 
-      const allOrders = await knex('pedidos');
+      const allOrders = await knex("pedidos");
       for (order of allOrders) {
-
-        const orderProducts = await knex('pedidos_produtos')
-          .where('pedido_id', order.id);
+        const orderProducts = await knex("pedidos_produtos").where(
+          "pedido_id",
+          order.id
+        );
 
         const orderOb = { pedido: order, pedido_produtos: orderProducts };
 
@@ -135,7 +138,7 @@ const listOrder = async (req, res) => {
       return res.status(200).json(formatedOrders);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json(error.message);
   }
 };
@@ -143,5 +146,4 @@ const listOrder = async (req, res) => {
 module.exports = {
   registerOrder,
   listOrder,
-}
-
+};
